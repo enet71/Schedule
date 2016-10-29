4e89bbb5989a6fbab0d17e3eb241d05b202a7dae
enet71 <enet71@gmail.com>
2016-10-29 15:56:54 +0300

    Added gitignore

--- src/main/java/db/StaticConfiguration.java
+++ src/main/java/db/StaticConfiguration.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.util.Calendar;
 
 public class StaticConfiguration{
-    public static int port = 49210;
+    public static int port = 52443;
     public static Connection connection = null;
     public static boolean work = false;
     public static Calendar calendar;
